da9cf549a84792f76bd294ebd62117875b58f01b
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-24 11:47:43 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 use std::{env, error::Error, sync::Arc};
-use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_cache_inmemory::InMemoryCache;
 use twilight_gateway::{Event, Intents, Shard, ShardId};
 use twilight_http::Client as HttpClient;
 use twilight_model::{
