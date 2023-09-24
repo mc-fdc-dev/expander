1b1084da78eec2b7562dc1135875827c4d974bb8
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-24 10:20:46 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use regex::Regex;
+use std::{env, error::Error, sync::Arc};
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::{Event, Intents, Shard, ShardId};
+use twilight_http::Client as HttpClient;
+use twilight_model::id::{
+    marker::{ChannelMarker, UserMarker},
+    Id,
+};
+use twilight_util::builder::embed::{EmbedAuthorBuilder, EmbedFooterBuilder, EmbedBuilder, ImageSource};
+
+struct ClientData {
+    re: Regex,
+}
+
+struct Client {
+    http: Arc<HttpClient>,
+    cache: Arc<InMemoryCache>,
+    data: Arc<ClientData>,
+}
+
+struct MessageData {
+    content: String,
+    author_id: Id<UserMarker>,
+    channel_name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let token = env::var("DISCORD_TOKEN")?;
+    let intents = Intents::GUILD_MESSAGES
+        | Intents::DIRECT_MESSAGES
+        | Intents::MESSAGE_CONTENT
+        | Intents::GUILD_MEMBERS;
+
+    let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+
+    let http = Arc::new(HttpClient::new(token));
+
+    let cache = Arc::new(
+        InMemoryCache::builder()
+            .resource_types(ResourceType::MESSAGE | ResourceType::USER | ResourceType::GUILD)
+            .build(),
+    );
+    let client = Arc::new(Client {
+        http: Arc::clone(&http),
+        cache: Arc::clone(&cache),
+        data: Arc::new(ClientData {
+            re: Regex::new(r"https://discord(app)?.com/channels/(\d+)/(\d+)/(\d+)").unwrap(),
+        }),
+    });
+
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(source) => {
+                if source.is_fatal() {
+                    break;
+                }
+
+                continue;
+            }
+        };
+        cache.update(&event);
+
+        tokio::spawn(handle_event(event, Arc::clone(&client)));
+    }
+
+    Ok(())
+}
+
+async fn handle_event(
+    event: Event,
+    client: Arc<Client>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) => {
+            if msg.author.bot {
+                return Ok(());
+            }
+            if let Some(caps) = client.data.re.captures(msg.content.as_str()) {
+                let channel_id = caps.get(3).unwrap().as_str().parse::<u64>().unwrap();
+                let message_id = caps.get(4).unwrap().as_str().parse::<u64>().unwrap();
+
+                let channel: Id<ChannelMarker> = Id::new(channel_id);
+                let message: Option<MessageData> = {
+                    if let Some(message) = client.cache.message(Id::new(message_id)) {
+                        let channel = client.cache.channel(channel).unwrap();
+                        Some(MessageData {
+                            content: message.content().clone().to_string(),
+                            author_id: message.author(),
+                            channel_name: channel.name.clone().unwrap(),
+                        })
+                    } else {
+                        let message = client.http.message(channel, Id::new(message_id)).await;
+                        if let Ok(message) = message {
+                            let target = message.model().await?;
+                            let channel = client.cache.channel(channel).unwrap();
+                            Some(MessageData {
+                                content: target.content.clone().to_string(),
+                                author_id: target.author.id,
+                                channel_name: channel.name.clone().unwrap(),
+                            })
+                        } else {
+                            None
+                        }
+                    }
+                };
+                let target = message.unwrap();
+                let author = client.cache.user(target.author_id).unwrap();
+                let avatar_url = {
+                    format!(
+                        "https://cdn.discordapp.com/avatars/{}/{}.png",
+                        author.id,
+                        author.avatar.as_ref().unwrap()
+                    )
+                };
+                let embed = EmbedBuilder::new()
+                    .description(target.content)
+                    .author(
+                        EmbedAuthorBuilder::new(author.name.clone())
+                            .icon_url(ImageSource::url(avatar_url).unwrap())
+                            .build(),
+                    )
+                    .footer(
+                        EmbedFooterBuilder::new(target.channel_name)
+                            .build()
+                    )
+                    .build();
+
+                client
+                    .http
+                    .create_message(channel)
+                    .embeds(&[embed])?
+                    .await?;
+            }
+        }
+        Event::Ready(_) => {
+            println!("Shard is ready");
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
