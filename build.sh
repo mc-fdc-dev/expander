4c39d46af4206be223dbd78b3193ea2ca21a3ecf
コマリン親衛隊 <contact@tuna2134.dev>
2024-02-04 02:08:14 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-if [ &TARGETARCH = "arm64" ]; then
-    CC_aarch64_unknown_linux_musl=clang;
-    AR_aarch64_unknown_linux_musl=llvm-ar;
-    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-Clink-self-contained=yes -Clinker=rust-lld";
+if [[ &TARGETARCH = "arm64" ]]; then
+    CC_aarch64_unknown_linux_musl=clang
+    AR_aarch64_unknown_linux_musl=llvm-ar
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-Clink-self-contained=yes -Clinker=rust-lld"
+else
+    echo "amd64 mode"
 fi
 cargo build --target=$(cat /tmp/arch)-unknown-linux-musl --release
 cp target/$(cat /tmp/arch)-unknown-linux-musl/release/expander /tmp/expander
