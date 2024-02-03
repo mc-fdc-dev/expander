cde4083d3015af86c9f056ece9809f7c62882d12
コマリン親衛隊 <contact@tuna2134.dev>
2024-02-04 02:02:25 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ if [ &TARGETARCH = "arm64" ]; then
     AR_aarch64_unknown_linux_musl=llvm-ar;
     CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-Clink-self-contained=yes -Clinker=rust-lld";
 fi
-cargo build --target=$(cat /tmp/arch)-unknown-linux-musl --release && \
+cargo build --target=$(cat /tmp/arch)-unknown-linux-musl --release
 cp target/$(cat /tmp/arch)-unknown-linux-musl/release/expander /tmp/expander
