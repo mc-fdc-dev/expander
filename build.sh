b7f2301a11358717104103dbf1007367cc9556e9
コマリン親衛隊 <contact@tuna2134.dev>
2024-02-04 02:11:23 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-if [[ &TARGETARCH = "arm64" ]]; then
+if [ &TARGETARCH = "arm64" ]; then
     CC_aarch64_unknown_linux_musl=clang
     AR_aarch64_unknown_linux_musl=llvm-ar
     CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-Clink-self-contained=yes -Clinker=rust-lld"
